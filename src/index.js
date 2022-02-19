6ed953a1fd3426cfb8f888e7e37293e60ade722b
Codeprince <anyaohaprince122@gmail.com>
2022-02-19 09:01:25 +0100

    added web socket

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
 
 const app = express();
+const server = http.createServer(app);
 const port = 3002;
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log("Listening on port " + port);
 });
