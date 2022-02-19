0a12ad7c64150cabc3e10595cfef865776271087
Codeprince <anyaohaprince122@gmail.com>
2022-02-19 08:47:42 +0100

    added start script

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = 3002;
 
-const publicDirectoryPath = path.join((__dirname, "../public"));
+const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
