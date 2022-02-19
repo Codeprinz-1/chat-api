8136e63f892f78d890abe01c8b2972aba7ebfa24
Codeprince <anyaohaprince122@gmail.com>
2022-02-19 09:17:24 +0100

    added contents of index.html

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const port = 3002;
+const io = socketio(server);
 
+const port = 3002;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
+
+io.on("connection", () => {
+  console.log("New Connection yay");
+});
 
 server.listen(port, () => {
   console.log("Listening on port " + port);
