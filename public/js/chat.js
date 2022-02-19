fd08367b4cf2d9ac350d8c86482868db576574c2
Codeprince <anyaohaprince122@gmail.com>
2022-02-19 10:13:44 +0100

    added send function

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,12 @@
 const socket = io();
 
-socket.on("message", (data) => {
+socket.on("connect", (data) => {
   console.log(data);
 });
+
+const send = function () {
+  const text = document.getElementById("message").value;
+  if (text) {
+    socket.emit("message", text);
+  }
+};
